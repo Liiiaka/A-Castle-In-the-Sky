11196621e1a5397c483bff3fc8d7aac63497a812
BuvyLony <63240739+BuvyLony@users.noreply.github.com>
2020-09-29 16:58:16 +0200

    Add files via upload

---
+++ Scripts/Falltuer_Script.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falltuer_Script : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject ButtonUI;
+ 
+     void Start()
+     {
+         anim = GetComponent<Animator>();
+     }
+ 
+  
+ 
+     private void OnTriggerEnter(Collider other)
+     {
+         if (other.tag == "Key")
+         {
+             //Debug.Log("Door unlocked");
+             Destroy(other.gameObject);
+             anim.SetTrigger("Open");
+             ButtonUI.SetActive(true);
+         }
+     }
+ }
---
+++ Scripts/L2Falltuer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L2Falltuer : MonoBehaviour
+{
+    private Animator anim;
+    public GameObject ButtonL2UI;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Key")
+        {
+            //Debug.Log("Door unlocked");
+            Destroy(other.gameObject);
+            anim.SetTrigger("Open");
+            ButtonL2UI.SetActive(true);
+        }
+    }
+}
